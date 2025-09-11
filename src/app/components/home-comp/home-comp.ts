b07e805458314846cba50b684bc06c3e55f3423c
julixnss <u20221f958@upc.edu.pe>
2025-09-11 15:31:18 -0500

    11/09

--- src/app/components/home-comp/home-comp.ts
+++ src/app/components/home-comp/home-comp.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-comp',
@@ -6,6 +6,32 @@ import { Component } from '@angular/core';
   templateUrl: './home-comp.html',
   styleUrl: './home-comp.css'
 })
-export class HomeComp {
+export class HomeComp implements OnInit, OnDestroy{
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  private intervalId: any;
+  startDate = new Date('2023-01-02T00:00:00');
+
+  ngOnInit() {
+    this.startCountUp();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  startCountUp() {
+    this.intervalId = setInterval(() => {
+      const now = new Date().getTime();
+      const elapsed = now - this.startDate.getTime();
+
+      this.days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((elapsed / (1000 * 60 * 60)) % 24);
+      this.minutes = Math.floor((elapsed / (1000 * 60)) % 60);
+      this.seconds = Math.floor((elapsed / 1000) % 60);
+    }, 1000);
+  }
 
 }
